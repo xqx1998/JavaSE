14e5a9ca63cf1e7ee7e40e8efd107a415d62f075
xqx1998 <2515658674@qq.com>
2019-07-16 10:31:54 +0800

    java第一个入门程HelloWorld！

---
+++ day01/src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * createAuthor£ºxingquanxiang
+ * createTime£º2019/7/16 10:23
+ * description:
+ */
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("Hello world !");
+    }
+}
